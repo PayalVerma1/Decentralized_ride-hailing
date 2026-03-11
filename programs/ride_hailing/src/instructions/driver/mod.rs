6dac735ba26194e50b47a26838465d996dc2096b
PayalVerma1 <payalvermaseema@gmail.com>
2026-03-11 23:58:38 +0530

    modularity improved

---
+++ programs/ride_hailing/src/instructions/driver/mod.rs
@@ -0,0 +1,7 @@
+pub mod register_driver;
+pub mod slash_driver;
+pub mod verify_driver;
+
+pub use register_driver::*;
+pub use slash_driver::*;
+pub use verify_driver::*;
--- programs/ride_hailing/src/instructions/mod.rs
+++ programs/ride_hailing/src/instructions/mod.rs
@@ -1,9 +1,5 @@
-pub mod register_driver;
-pub mod slash_driver;
-pub mod verify_driver;
+pub mod driver;
 pub mod ride;
 
-pub use register_driver::*;
-pub use slash_driver::*;
-pub use verify_driver::*;
+pub use driver::*;
 pub use ride::*;
